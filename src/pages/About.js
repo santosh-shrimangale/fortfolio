013639383c58b8ab705a215beebbb83c36017c60
santosh-shrimangale <santoshshrimangale01@gmaiil.com>
2023-11-22 00:17:16 +0530

    design  para in abouts us page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,11 +3,15 @@ import "./About.css"
 function About() {
   return (
     <div className='about-us'>
+    <p className='aboutPara'>about us</p>
       <h1 className="who">WHO IM I?</h1>
       <div className="para">
-        I'm Santosh, a passionate web development with a knack for [mention a key skill or quality, e.g., problem-solving]. With a background in [mention relevant education or experience], I thrive in dynamic environments and tackle challenges head-on.
-
-        <p> What sets me apart is my [mention unique quality, e.g., creativity] and my commitment to delivering exceptional results. When I'm not working, I enjoy [mention a personal interest or hobby, e.g., photography].</p>
+       <p> I'm Santosh,  I am passionate about building websites and online apps through their development cycle using my creative skills and technical knowledge.
+         With a background in Msc Comp science and Having 2 yers experience, I thrive in dynamic environments and tackle challenges head-on.
+        </p>
+        <p>Highly skilled and innovative Web Developer with 2 years of experience in designing and developing responsive,
+           user-friendly websites and web applications. Proficient in various programming languages, frameworks, and tools,
+         I bring a passion for creating efficient, scalable, and visually appealing digital solutions.</p>
       </div>
     </div>
   )
